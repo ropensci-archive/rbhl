fbd03c718ce4f9f2c4bf0416bbbf41e0568f1728
Scott Chamberlain <myrmecocystus@gmail.com>
2011-08-05 08:01:49 -0500

    added function bioherlib for all API methods for now

--- R/GetPageMetadata.R
+++ R/GetPageMetadata.R
@@ -1,61 +1,45 @@
-# GetPageMetadata.R
+# bioherlib.R
 
-url <- 'http://www.biodiversitylibrary.org/api2/httpquery.ashx?op=GetPageMetadata&pageid=1328690&ocr=t&names=t&format=json&apikey=ea5bbfda-e6ad-4fce-b288-70fa9410a9b3'
-fromJSON(url)
-
-GetPageMetadata <- 
-# Function htmlviews to summarise all pdf, html, and xml views
+bioherlib <- 
 # Args:
-#   doi: digital object identifier for an article in PLoS Journals  (string)
-#   source_ (character): Five categories of sources
-#     Usage stats: counter [PLoS' own stats], pmc [PubMed].
-#     Citations: pubmed, crossref, scopus, or wos [Web of Science].
-#     Social: citeulike.
-#     Blog entries: nature (no. of Nature blog articles that mentioned the article).
-#     Inactive sources: researchblogging, connotea, bloglines, postgenomic
-#   citations: include the individual citing document URIs, grouped by source,
-#     TRUE (T) or FALSE (F) (character)
-#   history: include a historical record of citation counts per month (cumulative), 
-#     grouped by source, TRUE (T) or FALSE (F) (character)
-#   downform: download format (json, xml or csv)
+#   method: API method: GetPageMetadata, GetPageOcrText, GetPageNames, 
+#     GetItemMetadata, GetItemByIdentifier, GetItemPages, GetUnpublishedItems,
+#     GetTitleMetadata, GetTitleItems, GetTitleByIdentifier, TitleSearchSimple,
+#     GetTitleBibTex, GetTitleEndNote, GetUnpublishedTitles, SubjectSearch,
+#     GetSubjectTitles, AuthorSearch, GetAuthorTitles, NameCount, NameList,
+#     NameGetDetail, NameSearch, GetCollections, GetLanguages
+#   page: page number to get
+#   ocrtext: return ocr text of the page (TRUE/FALSE)
+#   names: return the names that appear on the page (TRUE/FALSE)
+#   format: json or xml (character)
 # Examples: 
-#   plosallviews('10.1371/journal.pbio.0000012', 'counter', T, T, 'xml')
-#   plosallviews('10.1371/journal.pbio.0000012', 'citeulike', T, T, 'json')
-#   plosallviews('10.1371/journal.pone.0005723', 'counter', T, T, 'csv')
+#   bioherlib(method = 'GetTitleMetadata', page = '1328690', format = 'json')
+#   bioherlib('GetPageMetadata', '1328690', 'TRUE', 'TRUE', 'json')
+#   bioherlib('GetPageNames', '1328690', 'TRUE', 'TRUE', 'json')
 
-function(doi, source_ = NA, citations = FALSE, history = FALSE, downform = NA,
-  url = 'http://alm.plos.org/articles',
-  key = getOption("PlosApiKey", stop("need an API key for PLoS Journals")),
+function(method = NA, page = NA, ocrtext = FALSE, names = FALSE, format = NA,
+  url = 'http://www.biodiversitylibrary.org/api2/httpquery.ashx',
+  key = getOption("BioHerLibKey", stop("need an API key for the Biod Her Library")),
   ..., 
   curl = getCurlHandle() ) {
 
-  if(! downform == 'csv') {  
-    url2 <- paste(url, "/", doi, '.', downform, sep='')
-    args <- list(api_key = key)
-    if(!is.na(source_))
-      args$source <- source_
-    if(!citations == FALSE)
-      args$citations <- 1
-    if(!history == FALSE)
-      args$history <- 1
-    tt <- getForm(url2, 
-      .params = args, 
-      ..., 
-      curl = curl)
-    if(downform == 'json') {outprod <- fromJSON(I(tt))} else
-      if(downform == 'xml') {outprod <- xmlTreeParse(I(tt))}
-  } else
-  outprod <- cat("No support for CSV downloads at the moment - apologies")
-#   { if(is.na(source_)) {source2 <- NULL} else
-#       {source2 <- paste('source=', source_, sep='')}
-#     if(citations == FALSE) {citations2 <- NULL} else
-#       {citations2<-paste('&citations=1')}
-#     if(history == FALSE) {history2 <- NULL}
-#       {history2 <- paste('&history=1')}
-#     urlcsv <- paste(url, "/", doi, '.', downform, '?', source2, 
-#       citations2, history2, '&api_key=', key, sep='')
-#     outprod <- read.csv(urlcsv)
-#   }
-  
-return(outprod)
+  args <- list(op = method, apikey = key)
+  if(!is.na(method))
+    args$op <- method
+  if(!is.na(page))
+    args$pageid <- page
+  if(!is.na(format))
+    args$format <- format
+  if(ocrtext == 'TRUE') {args$ocr <- 't'} else
+    if(ocrtext == 'FALSE') {args$ocr <- NULL}
+  if(names == 'TRUE') {args$names <- 't'} else
+    if(names == 'FALSE') {args$names <- NULL}
+
+  tt <- getForm(url, 
+    .params = args, 
+    ..., 
+    curl = curl)
+  if(format == 'json') {outprod <- fromJSON(I(tt))} else
+      if(format == 'xml') {outprod <- xmlTreeParse(I(tt))}
+  return(outprod)
 }
