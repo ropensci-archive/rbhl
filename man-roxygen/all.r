f4f1b432f26090f59cf98dd255ecc7fdc4194886
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-20 13:12:21 -0700

    remade man files with new roxygen2 ver updatd date bumped version to 0.1.2 added Roxygen: list(wrap = FALSE) to DESCRIPTION file

--- man-roxygen/all.r
+++ man-roxygen/all.r
@@ -2,7 +2,5 @@
 #' 		format is forced to equal 'json'
 #' @param key your BHL API key, either enter, or loads from .Rprofile
 #' @param callopts Call options passed on to httr::GET.
-#' @param output Return a list, raw json or xml, or parsed data (character). 
+#' @param output Return a list, raw json or xml, or parsed data (character).
 #'    Default: 'list'
-#' @param key your BHL API key, either enter, or loads from .Rprofile
-#' @param callopts Call options passed on to httr::GET.
