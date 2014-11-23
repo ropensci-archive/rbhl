a381730c95084ea920608e5a4b2e5ad05c9aaefc
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-23 13:27:17 -0800

    fixed broken test

--- tests/testthat/test-bhl_getlanguages.R
+++ tests/testthat/test-bhl_getlanguages.R
@@ -12,12 +12,12 @@ zz <- bhl_getlanguages(output='raw', format='xml')
 test_that("bhl_getlanguages returns the correct class", {
   expect_is(tt, "list")
   expect_is(tt$Result, "list")
-  
+
   expect_is(uu, "data.frame")
-  
+
   expect_is(vv, "character")
   expect_is(fromJSON(vv), "list")
-  
+
   expect_is(zz, "character")
   expect_is(xmlParse(zz), "XMLInternalDocument")
 })
@@ -25,7 +25,7 @@ test_that("bhl_getlanguages returns the correct class", {
 test_that("bhl_getlanguages returns the correct dimensions", {
   expect_equal(length(tt), 3)
   expect_equal(length(tt$Status), 1)
-  expect_equal(nrow(uu), 64)
+  expect_equal(NCOL(uu), 2)
   expect_equal(length(fromJSON(vv)), 3)
   expect_equal(length(zz), 1)
   expect_equal(length(xmlParse(zz)), 1)
