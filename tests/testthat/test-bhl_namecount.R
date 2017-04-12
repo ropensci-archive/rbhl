68632b677b01f8f348e69dbf225cce36aa3b629c
Scott Chamberlain <myrmecocystus@gmail.com>
2017-04-12 13:28:46 -0700

    fix a test

--- tests/testthat/test-bhl_namecount.R
+++ tests/testthat/test-bhl_namecount.R
@@ -16,6 +16,4 @@ test_that("bhl_namecount works", {
   # the correct dimensions
   expect_equal(length(zz), 1)
   expect_equal(length(jsonlite::fromJSON(zz)), 3)
-  expect_equal(length(vv), 1)
-  expect_equal(length(xml2::read_xml(vv)), 2)
 })
